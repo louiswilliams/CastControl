39ce2e322aac6ae70842a5a0a471e9c85bf14f91
extrakt <louieaw@gmail.com>
2013-09-10 15:00:06 -0400

    Initial Commit

---
+++ src/com/extrakt/castcontrol/MainActivity.java
@@ -0,0 +1,34 @@
+package com.extrakt.castcontrol;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+import com.google.cast.CastContext;
+import com.google.cast.CastDevice;
+import com.google.cast.MediaRouteAdapter;
+import com.google.cast.MediaRouteHelper;
+import com.google.cast.MediaRouteStateChangeListener;
+
+
+import android.support.v4.app.FragmentActivity;
+import android.support.v7.app.MediaRouteButton;
+import android.support.v7.media.MediaRouteSelector;
+import android.support.v7.media.MediaRouter;
+import android.support.v7.media.MediaRouter.RouteInfo;
+
+public class MainActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+
+}
